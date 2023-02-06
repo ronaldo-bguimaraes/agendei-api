ba4c0e5f19433b6fb72bdd7ca6655a27ce65132d
Ronaldo Guimarães <ronaldobgui@outlook.com>
2023-02-05 21:13:30 -0400

    Arruma README.md

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-## agendei-api
- 
+# Agendei API
 Para executar o projeto basta ter o docker instalado e executar o seguinte comando:
 
 - Windows
@@ -11,3 +10,7 @@ docker compose up --build
 ```commandline
 sudo docker compose up --build
 ```
+
+## Configurações de ambiente
+- Docker
+- Python 3.10
--- database.py
+++ database.py
@@ -8,7 +8,7 @@ session = sessionmaker(bind=engine, future=True)
 
 def get_hour():
     with engine.connect() as connection:
-        script = text()
+        script = text('select current_timestamp(6)')
         result = connection.execute(script)
         result = result.fetchone()
         return result[0].__str__()
